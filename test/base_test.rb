c1e766ec4898a425dc4b94edbd44d0caad19b5b6
morgen-peschke <morgen.peschke@gmail.com>
2014-08-01 15:22:51 -0400

    Separate base tests

---
+++ test/base_test.rb
@@ -0,0 +1,72 @@
+require 'minitest/autorun'
+require_relative '../lib/parsed/base'
+
+# A parser that doesn't parse for testing
+module Parsed
+  module TestParser
+    include Parsed::Base
+    def stupid_attribute(name)
+      __define_parsed_attributes_all_methods name do |raw_value|
+        "stupid #{raw_value}"
+      end        
+    end
+
+    def broken_attribute(name, options = {})
+      __define_parsed_attributes_all_methods name, options do |raw_value|
+        1/0
+      end
+    end     
+  end
+end
+
+class BaseTest < MiniTest::Test
+
+  class Wrapper
+    extend Parsed::TestParser
+    stupid_attribute :name
+    broken_attribute :raises_never
+    broken_attribute :raises_once,   raise:        true
+    broken_attribute :raises_always, raise_always: true
+  end
+
+  def setup
+    super
+    @test_obj = Wrapper.new
+  end
+
+  def test_attribute_creation
+    %w(name raises_never raises_once raises_always).each do |name|
+      assert  @test_obj.respond_to?("raw_#{name}"),    "Missing raw_#{name}"
+      assert  @test_obj.respond_to?("raw_#{name}="),   "Missing raw_#{name}="
+      assert  @test_obj.respond_to?("parsed_#{name}"), "Missing parsed_#{name}"
+      assert !@test_obj.respond_to?('pared_#{name}='), "Shouldn't define parsed_#{name}="
+    end
+  end
+
+  def test_raw
+    @test_obj.instance_variable_set('@parsed_name', 'cat')
+    @test_obj.raw_name = 'cat'
+
+    assert_equal 'cat', @test_obj.instance_variable_get('@raw_name'),         'Raw setter failed'
+    assert_equal 'cat', @test_obj.raw_name,                                   'Raw getter failed'
+    assert_equal false,  @test_obj.instance_variable_defined?('@parsed_name'), 'Parsed not unset'
+  end
+
+  def test_parsed
+    @test_obj.raw_name = 'cat'
+
+    assert_equal 'stupid cat', @test_obj.parsed_name, 'Parsed getter failed'
+
+    @test_obj.raw_raises_never  = 'irrelevant'
+    @test_obj.raw_raises_once   = 'irrelevant'
+    @test_obj.raw_raises_always = 'irrelevant'
+    
+    assert_equal nil, @test_obj.parsed_raises_never, 'Failure should set parsed to nil'
+
+    assert_raises(ZeroDivisionError, 'Should have raised the first time') {@test_obj.parsed_raises_once}
+    assert_nil @test_obj.parsed_raises_once, 'Parse should not attempt again until raw is reset when raise_always option is not set'
+    
+    assert_raises(ZeroDivisionError, 'Should have raised the first time')  {@test_obj.parsed_raises_always}
+    assert_raises(ZeroDivisionError, 'Should have raised the second time') {@test_obj.parsed_raises_always}
+  end
+end
--- test/json_test.rb
+++ test/json_test.rb
@@ -19,49 +19,28 @@ class JsonTest < MiniTest::Test
     @simple_parsed = {'a' => 1}
   end
   
-  def test_attribute_creation
-    %w(raw_json raw_json= parsed_json).each do |attr|
-      assert @test_obj.respond_to?(attr), "Missing #{attr}"
-    end
-    assert !@test_obj.respond_to?('parsed_json='), "Shouldn't define parsed setter"
-  end
-
-  def test_raw
-    @test_obj.instance_variable_set('@parsed_json', {'b' => 2})
-    @test_obj.raw_json = @simple_raw
-
-    assert_equal @simple_raw, @test_obj.instance_variable_get('@raw_json'),         'Raw setter failed'
-    assert_equal @simple_raw, @test_obj.raw_json,                                   'Raw getter failed'
-    assert_equal false,       @test_obj.instance_variable_defined?('@parsed_json'), 'Parsed not unset'
-  end
+  
 
   def test_parsed
-    @test_obj.raw_json = @simple_raw
-
+    @test_obj.raw_json        = @simple_raw
+    
     assert_equal @simple_parsed, @test_obj.parsed_json, 'Parsed getter failed'
 
-    @test_obj.raw_json = 'This is clearly not JSON'
-
+    @test_obj.raw_json          = 'This is clearly not JSON'
+    @test_obj.raw_raises_once   = 'This is clearly not JSON'
+    @test_obj.raw_raises_always = 'This is clearly not JSON'
+    
     assert_equal nil, @test_obj.parsed_json, 'Failure should set to parsed nil'
 
-    exception = assert_raises(JSON::ParserError) do
-      @test_obj.raw_raises_once = 'This is clearly not JSON'
-      @test_obj.parsed_raises_once
-    end
+    exception = assert_raises(JSON::ParserError, 'Should have raised the first time') {@test_obj.parsed_raises_once}
     assert exception.message.match(/unexpected token/), 'Unexpected parser error message'
 
     assert_nil @test_obj.parsed_raises_once, 'Parse should not attempt again until raw is reset when raise_always option is not set'
 
-    exception = assert_raises(JSON::ParserError) do
-      @test_obj.raw_raises_always = 'This is clearly not JSON'
-      @test_obj.parsed_raises_always
-    end
+    exception = assert_raises(JSON::ParserError, 'Should have raised the first time') {@test_obj.parsed_raises_always}
     assert exception.message.match(/unexpected token/), 'Unexpected parser error message'
 
-    exception = assert_raises(JSON::ParserError) do
-      @test_obj.raw_raises_always = 'This is clearly not JSON'
-      @test_obj.parsed_raises_always
-    end
+    exception = assert_raises(JSON::ParserError, 'Should have raised the second time') {@test_obj.parsed_raises_always}
     assert exception.message.match(/unexpected token/), 'Unexpected parser error message'
   end
     
